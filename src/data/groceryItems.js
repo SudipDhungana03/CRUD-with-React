a58eeff02f5e4b0f569269fee0b30ffa3c0fc3d5
SudipDhungana03 <sudipdhungana2063@gmail.com>
2026-02-18 21:28:38 +0545

    Adding groceryItems.js with initial grocery list

---
+++ src/data/groceryItems.js
@@ -0,0 +1,8 @@
+const groceryItems = [
+  { id: 1, name: 'Milk', completed: false },
+  { id: 2, name: 'Bread', completed: true },
+  { id: 3, name: 'Eggs', completed: false },
+  { id: 4, name: 'Apples', completed: false },
+];
+
+export default groceryItems;
